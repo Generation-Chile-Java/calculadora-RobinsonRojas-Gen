9ff2637a5cb0eb438bacd8ba80c870afd023dfe2
RobinsonRojas-Gen <robinsonrojasreyes@gmail.com>
2025-06-13 14:32:08 -0400

    Correciones de texto y condicionamiento de la división al calcular resto

--- CalculadoraJava/src/Calculadora.java
+++ CalculadoraJava/src/Calculadora.java
@@ -9,8 +9,8 @@ public class Calculadora {
         System.out.println("\nBienvenido a la calculadora Java, que procesa sumas, restas, multiplicación, divisiones y calcula restos de divisiones"); // Saluda
 
         System.out.println("\nEsta calculadora funciona con el ingreso de 2 números, suma estos 2 números, resta el segundo número del primero, "); // Explica lo que hace el programa
-        System.out.println("multiplica ámbos números e imprime el resultado, entrega el cociente de una división (el primer número dividido por el segundo), ");
-        System.out.println("y así también entrega el resto del primer número dividido por el segundo");
+        System.out.println("multiplica ámbos números e imprime el resultado, entrega el cociente de una división (del primer número dividido por el segundo), ");
+        System.out.println("y también entrega el resto del primer número dividido por el segundo, resultando en otro valor esta división");
 
         System.out.println("\n[Ingrese el primer número]: "); // Pide ingreso de 2 válores
         int primerNumero = ingresoTeclado.nextInt();
@@ -26,8 +26,8 @@ public class Calculadora {
         // Imprime división y resto juntos (pegados)
 
         float divisionDeAmbosNumerosIngresados = (float)primerNumero / segundoNumero; // Hace posible resultado de división con decimales
-        System.out.println("\nEl cociente, del resultado de la división del primer número ingresado por el segundo en ingresar es: " + primerNumero + " / " + segundoNumero + " = " + divisionDeAmbosNumerosIngresados);
-        System.out.println("Y el resto de esta división es: " + (primerNumero % segundoNumero));
+        System.out.println("\nEl cociente, resultado de la división del primer número ingresado por el segundo en ingresar es: " + primerNumero + " / " + segundoNumero + " = " + divisionDeAmbosNumerosIngresados);
+        System.out.println("Y el resto de esta división es: " + (primerNumero % segundoNumero) + ", en este caso, el resultado de la división con resto es: " + (primerNumero / segundoNumero));
 
     }
 }
